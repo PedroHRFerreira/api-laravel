6218f78f458c66315c6144b646ceeb32f7b3df1a
PedroHRFerreira <devpedrohr@gmail.com>
2025-05-31 14:45:01 -0300

    docs: :memo: rotas

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
 Route::resource('/produtos', ProdutoController::class);
+
+// Para verificar a rotas disponiveis digite no terminal: php artisan route:list, com isso listará todas as rotas.
