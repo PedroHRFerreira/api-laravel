f3ce6cc10510210cecae9751935375e13f0bc7c4
PedroHRFerreira <devpedrohr@gmail.com>
2025-06-01 22:45:26 -0300

    fix: :art: AJustando formato de testes

--- tests/Feature/produtoTest.php
+++ tests/Feature/produtoTest.php
@@ -2,79 +2,59 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Produto;
 
 class produtoTest extends TestCase
 {
-    // public function test_example(): void
-    // {
-    //      /// teste de rota de GET ou a function de index do controller produto.
+    public function test_get_produto(): void
+    {
+         /// teste de rota de GET ou a function de index do controller produto.
 
-    //     $produtos = Produto::create([
-    //         'nome' => 'Teste',
-    //         'preco' => 100.00,
-    //         'descricao' => 'Produto de teste',
-    //         'type' => 'eletronico'
-    //      ]);
+        $produtos = Produto::create([
+            'nome' => 'Teste',
+            'preco' => 100.00,
+            'descricao' => 'Produto de teste',
+            'type' => 'eletronico'
+         ]);
 
-    //      // Primeiro eu estou criando um produto usando o metodo do eloquent create, e fazendo um criação padrão para o teste.
+         // Primeiro eu estou criando um produto usando o metodo do eloquent create, e fazendo um criação padrão para o teste.
 
-    //     $response = $this->get('/');
-    //     // Aqui estou fazendo uma requisição GET para a rota raiz do aplicativo, que deve estar vinculada ao método index do ProdutoController.
+        $response = $this->get('/');
+        // Aqui estou fazendo uma requisição GET para a rota raiz do aplicativo, que deve estar vinculada ao método index do ProdutoController.
 
-    //     $response->assertStatus(200);
-    //     // Já aqui estou verificando se a resposta da requisição tem o status HTTP 200, indicando que a requisição foi bem-sucedida.
+        $response->assertStatus(200);
+        // Já aqui estou verificando se a resposta da requisição tem o status HTTP 200, indicando que a requisição foi bem-sucedida.
 
 
-    //     dump($produtos);
-    //     // Utilizando o comando dump para exibir o conteúdo da variável $produtos na saída do PHP.
-    // }
-    // public function test_post_produto(): void
-    // {
-    //     // teste de rota de POST ou a function de store do controller produto.
-    //      $response = Produto::create([
-    //         'nome' => 'Teste',
-    //         'preco' => 100.00,
-    //         'descricao' => 'Produto de teste',
-    //         'type' => 'eletronico',
-    //      ]);
+        dump($produtos);
+        // Utilizando o comando dump para exibir o conteúdo da variável $produtos na saída do PHP.
+    }
+    public function test_post_produto(): void
+    {
+        // teste de rota de POST ou a function de store do controller produto.
+         $response = Produto::create([
+            'nome' => 'Teste',
+            'preco' => 100.00,
+            'descricao' => 'Produto de teste',
+            'type' => 'eletronico',
+         ]);
 
-    //      // Aqui estou criando um novo produto usando o método create do Eloquent, que é uma maneira de inserir dados no banco de dados.
+         // Aqui estou criando um novo produto usando o método create do Eloquent, que é uma maneira de inserir dados no banco de dados.
 
-    //         $this->assertDatabaseHas('produtos', [
-    //             'nome' => 'Teste',
-    //             'preco' => 100.00,
-    //             'descricao' => 'Produto de teste',
-    //             'type' => 'eletronico',
-    //         ]);
+            $this->assertDatabaseHas('produtos', [
+                'nome' => 'Teste',
+                'preco' => 100.00,
+                'descricao' => 'Produto de teste',
+                'type' => 'eletronico',
+            ]);
 
-    //     // Aqui estou verificando se o produto foi adicionado ao banco de dados, usando o método assertDatabaseHas do PHPUnit, que verifica se a tabela 'produtos' contém os dados especificados.
+        // Aqui estou verificando se o produto foi adicionado ao banco de dados, usando o método assertDatabaseHas do PHPUnit, que verifica se a tabela 'produtos' contém os dados especificados.
 
-    //     $this->assertTrue($response->wasRecentlyCreated, 'Produto não foi criado com sucesso.');
+        $this->assertTrue($response->wasRecentlyCreated, 'Produto não foi criado com sucesso.');
 
-    //     // Por ultimo, estou verificando se o produto foi criado recentemente, usando o método wasRecentlyCreated do Eloquent, que retorna verdadeiro se o modelo foi criado na última operação de inserção. Se não foi criado com sucesso, uma mensagem de erro será exibida.
-    // }
-
-    // function test_delete_produto(): void
-    // {
-    //     $produto = Produto::create([
-    //         'nome' => 'Teste',
-    //         'preco' => 100.00,
-    //         'descricao' => 'Produto de teste',
-    //         'type' => 'eletronico',
-    //      ]);
-
-    //     $produto->delete();
-
-    //     // Aqui estou criando um produto e, em seguida, excluindo-o usando o método delete do Eloquent.
-
-    //     $this->assertTrue($produto->wasRecentlyCreated, 'Produto não foi excluído com sucesso.');
-
-    //     // Por fim, estou verificando se o produto foi excluído com sucesso, se não foi excluído, uma mensagem de erro será exibida.
-    // }
+        // Por ultimo, estou verificando se o produto foi criado recentemente, usando o método wasRecentlyCreated do Eloquent, que retorna verdadeiro se o modelo foi criado na última operação de inserção. Se não foi criado com sucesso, uma mensagem de erro será exibida.
+    }
 
     public function test_update_produto(): void
     {
@@ -106,6 +86,24 @@ class produtoTest extends TestCase
         dump($produto);
 
         // Adicionado o comando dump para exibir o conteúdo da variável $produto na saída do PHP, foi pensado que ajudaria na depuração do teste.
+    }
+
+    function test_delete_produto(): void
+    {
+        $produto = Produto::create([
+            'nome' => 'Teste',
+            'preco' => 100.00,
+            'descricao' => 'Produto de teste',
+            'type' => 'eletronico',
+         ]);
+
+        $produto->delete();
+
+        // Aqui estou criando um produto e, em seguida, excluindo-o usando o método delete do Eloquent.
+
+        $this->assertTrue($produto->wasRecentlyCreated, 'Produto não foi excluído com sucesso.');
+
+        // Por fim, estou verificando se o produto foi excluído com sucesso, se não foi excluído, uma mensagem de erro será exibida.
     }
 
     // Para testa especificamente a rota de produto e este teste só rodar no terminal php artisan test --filter ProdutoTest
